13340f9f1f943a36f613cd4d26fb2f7b1ab79993
gruendlich <adrian.grund@student.hpi.de>
2022-10-31 16:57:26 +0100

    Issue 4: Implemented name concatenation

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
   it 'should store an authors firstname, lastname and homepage' do
     author = Author.create({first_name: 'Alan', last_name: 'Turing', homepage:'http://wikipedia.org/Alan_Turing'})
-    expect(author.first_name).to eq("Alan")
+    expect(author.first_name).to eq('Alan')
     expect(author.last_name).to eq('Turing')
     expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+  end
+
+  it 'should be able to return the concatenated name' do
+    author = Author.create({first_name: 'Alan', last_name: 'Turing', homepage:'http://wikipedia.org/Alan_Turing'})
+    expect(author.name).to eq('Alan Turing')
   end
 end
