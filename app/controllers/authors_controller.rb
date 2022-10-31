789c05af424a879ab898cfd234c97c083674cbf5
gruendlich <adrian.grund@student.hpi.de>
2022-10-31 16:33:54 +0100

    Issue 3 implemented Author Model

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,6 +2,17 @@ class AuthorsController < ApplicationController
   def new
   end
   def index
+  end
 
+  def create
+    @article = Author.new_with(author_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :homepage)
   end
 end
