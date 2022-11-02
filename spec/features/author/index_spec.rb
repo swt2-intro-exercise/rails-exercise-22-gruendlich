96cc95c8646da1b06230e9b6a058bea4198c2932
gruendlich <adrian.grund@student.hpi.de>
2022-11-02 10:45:07 +0100

    Issue: 7 Columns Many Tests not implemented of faulty

--- spec/features/author/index_spec.rb
+++ spec/features/author/index_spec.rb
@@ -2,12 +2,36 @@ require "rails_helper"
 
 describe "Authors list page" do
   it "should display all stored authors" do
+    visit '/authors'
 
+    # Test doesn't work!
+
+    Author.all.each { |a|
+      puts a
+      expect(page).to have_css('table', text: 'pappnase')
+      # within 'table' do
+      #   expect(page).to have_text a.first_name.to_s
+      #   expect(page).to have_text a.last_name.to_s
+      #   expect(page).to have_text a.homepage.to_s
+      #
+      # end
+    }
   end
 
   it "should provide a link to the authors creation page" do
     visit '/authors'
     expect(page).to have_link 'Create new Author', href: new_author_path
+  end
+
+  it "should provide a link to each authors index page" do
+    visit '/authors'
+    # Doesn't work
+    Author.all.each { |a|
+      expect(page).to have_link "Show", href: a.id
+    }
+    # within 'table' do
+    #   expect(page).to have_link "Show", href:
+    # end
   end
 end
 
