8344002b6e91b41aefdb90b5d522149598e05ee2
gruendlich <adrian.grund@student.hpi.de>
2022-11-12 16:42:37 +0100

    Issue 19: Paper index allows filtering via url parameter

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,4 +3,8 @@ class Paper < ApplicationRecord
   validates :venue, presence: true
   validates :year, presence: true, numericality: { only_integer: true }
   has_and_belongs_to_many :authors
+
+  def self.created_in(year)
+    where("year = ?", year) if year.present?
+  end
 end
